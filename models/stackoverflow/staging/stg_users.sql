637c15beee8984de0f1c66f19ca20a82fbf3bdaa
Kon <katenwankwo6@gmail.com>
2025-10-28 22:56:09 +0000

    saved changes

---
+++ models/stackoverflow/staging/stg_users.sql
@@ -0,0 +1,17 @@
+
+
+SELECT id as user_id,
+       display_name,
+       about_me as user_info,
+       age,
+       creation_date,
+       last_access_date,
+    --    CAST(creation_date AS TIMESTAMP WITH TIME ZONE) AS creation_date,
+    --    CAST(last_access_date AS TIMESTAMP WITH TIME ZONE) AS last_access_date,
+       location,
+       reputation,
+       up_votes,
+       down_votes,
+       views
+       
+FROM bigquery-public-data.stackoverflow.users

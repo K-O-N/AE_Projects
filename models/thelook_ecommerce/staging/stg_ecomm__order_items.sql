084209c525349decccfdf9b4d01ef3c3e052bd68
Kate Nwankwo <katenwankwo6@gmail.com>
2026-02-04 16:44:42 +0000

    commit changes to thelook ecommerce

--- models/thelook_ecommerce/staging/stg_ecomm__order_items.sql
+++ models/thelook_ecommerce/staging/stg_ecomm__order_items.sql
@@ -11,10 +11,10 @@ select id,
        product_id,
        inventory_item_id,
        status as order_status,
-       created_at,
-       shipped_at,
-       delivered_at,
-       returned_at,
-       sale_price
+       FORMAT_DATETIME('%Y-%m-%d %H:%M:%S', DATETIME(TIMESTAMP(created_at))) as created_at,
+       FORMAT_DATETIME('%Y-%m-%d %H:%M:%S', DATETIME(TIMESTAMP(shipped_at))) as shipped_at,
+       FORMAT_DATETIME('%Y-%m-%d %H:%M:%S', DATETIME(TIMESTAMP(delivered_at))) as delivered_at,
+       FORMAT_DATETIME('%Y-%m-%d %H:%M:%S', DATETIME(TIMESTAMP(returned_at))) as returned_at,
+       ROUND(sale_price, 4) as sale_price
 
 from order_stg
